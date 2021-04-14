493e8049f6b035261f3d10f3cfaa868bb21f1a22
Peter Mucha <pmucha@ebay.com>
2021-04-14 11:06:38 +0200

    fixed ports for ITs

--- jemini-example/src/test/java/com/github/warmuuh/jemini/example/GeminiProtocolIT.java
+++ jemini-example/src/test/java/com/github/warmuuh/jemini/example/GeminiProtocolIT.java
@@ -9,13 +9,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = Application.class,
+    webEnvironment = WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 class GeminiProtocolIT {
 
   private final GeminiClient client = GeminiClient.of("gemini://localhost");
